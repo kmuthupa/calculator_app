04b050568432e97e9f42036da3ea0dd4cbed9ed7
Karthik Muthupalaniappan <karthik.swaminathan@gmail.com>
2013-10-07 22:47:12 -0400

    Test drive a simple calculator prototype

--- app/javascripts/main.js
+++ app/javascripts/main.js
@@ -1,4 +1,30 @@
-  function init() {
-
-
+var Calculator = function(){
+  var Constructor = function(){
+    this.lastValues = [];
+    this.saveValues = function(value1, value2) {
+      this.lastValues.push(value1);
+      this.lastValues.push(value2);
+    };
   };
+
+  Constructor.prototype = {
+    add: function(value1, value2) {
+      this.saveValues(value1, value2);
+      return value1 + value2;
+    },
+    subtract: function(value1, value2) {
+      this.saveValues(value1, value2);
+      return value1 - value2;
+    },
+    multiply: function(value1, value2) {
+      this.saveValues(value1, value2);
+      return value1 * value2;
+    },
+    clear: function() {
+      this.lastValues.pop();
+      this.lastValues.pop();
+    },
+  };
+
+  return Constructor;
+}();
--- spec/main_spec.js
+++ spec/main_spec.js
@@ -1,7 +1,41 @@
-describe("The landing page", function(){
-  it("should display the greeting message when the page loads", function(){
-    init();
-    $greetingSpan = $("#greeting");
-    expect($greetingSpan).toHaveText("Hi. Hello!");
+describe("Calculator", function(){
+  var calculator;
+
+  beforeEach(function() {
+    calculator =  new Calculator();
+  });
+
+  it("stores nothing initially", function(){
+    var lastValues = calculator.lastValues;
+    expect(lastValues).toEqual([]);
+  });
+  
+  it("adds two numbers", function(){
+    var sum = calculator.add(1, 5);
+    expect(sum).toEqual(6);
+  });
+  
+  it("subtracts two numbers", function(){
+    var difference = calculator.subtract(6, 5);
+    expect(difference).toEqual(1);
+  });
+  
+  it("multiplies two numbers", function(){
+    var product = calculator.multiply(6, 5);
+    expect(product).toEqual(30);
+  });
+
+  it("saves numbers passed in", function(){
+    calculator.add(6, 7);
+    var lastValues = calculator.lastValues;
+    expect(lastValues).toContain(6);
+    expect(lastValues).toContain(7);
+  });
+  
+  it("clears the numbers", function(){
+    calculator.add(6, 7);
+    expect(calculator.lastValues).toEqual([6, 7]);
+    calculator.clear();
+    expect(calculator.lastValues).toEqual([]);
   });
 });
